a94c4b1253bb819c3071297d11b8279ecdb6f2b4
wanderleyjunior000 <wanderley.junior@skf.com>
2025-07-11 22:14:48 -0300

    Mensagem descritiva das suas alterações

--- meu_projeto_servico/settings.py
+++ meu_projeto_servico/settings.py
@@ -26,9 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7_c1ij_xocq_f-&%54m+q5zjx4*7xsy_ez%@)%(9x454#$=ds_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['assistencia-tecnica-django.onrender.com',
+                 '127.0.0.1', 'localhost']  # <--- MUDANÇA AQUI: Adicione seu domínio
 
 
 # Application definition
