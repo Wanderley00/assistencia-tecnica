30fb3e13460a8ef5e4afca9f340e4178c38778fa
wanderleyjunior000 <wanderley.junior@skf.com>
2025-07-21 00:12:23 -0300

    Mensagem descritiva das minhas alterações

--- meu_projeto_servico/settings.py
+++ meu_projeto_servico/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7_c1ij_xocq_f-&%54m+q5zjx4*7xsy_ez%@)%(9x454#$=ds_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['assistencia-tecnica-django.onrender.com',
                  '127.0.0.1', 'localhost']  # <--- MUDANÇA AQUI: Adicione seu domínio
@@ -142,6 +142,9 @@ LOCALE_PATHS = [
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Garante que sua pasta 'static' raiz seja incluída
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
