bfcef992845d641dc51234737115f25cb37c0e68
wanderleyjunior000 <wanderley.junior@skf.com>
2025-08-21 16:29:54 -0300

    Versão conectado com app mobile com função de relatórios OK

--- meu_projeto_servico/settings.py
+++ meu_projeto_servico/settings.py
@@ -33,10 +33,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7_c1ij_xocq_f-&%54m+q5zjx4*7xsy_ez%@)%(9x454#$=ds_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['assistencia-tecnica-django.onrender.com',
-                 '127.0.0.1', '192.168.18.8', '10.160.212.16', 'localhost']
+                 '127.0.0.1', 'localhost']
 
 
 # Application definition
