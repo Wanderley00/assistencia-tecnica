1f5cf03bb9fbfe3ffb689d9c5d750c25e6b15b43
wanderleyjunior000 <wanderley.junior@skf.com>
2025-09-26 11:29:34 -0300

    Melhorias aplicadas no sistema(Notificações no site, tela de análise de horas e correções de segurança 5)

--- meu_projeto_servico/settings.py
+++ meu_projeto_servico/settings.py
@@ -33,9 +33,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['assistencia-tecnica-django.onrender.com']
+ALLOWED_HOSTS = ['assistencia-tecnica-django.onrender.com', '127.0.0.1']
 
 
 # Application definition
