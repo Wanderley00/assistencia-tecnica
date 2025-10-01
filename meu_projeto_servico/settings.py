fcd9939cbfacdd4814ef8174e44a8a7795a3dd98
wanderleyjunior000 <wanderley.junior@skf.com>
2025-10-01 10:55:50 -0300

    Melhoria do registro de ponto no app mobile

--- meu_projeto_servico/settings.py
+++ meu_projeto_servico/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['assistencia-tecnica-django.onrender.com', '127.0.0.1', '*']
 
